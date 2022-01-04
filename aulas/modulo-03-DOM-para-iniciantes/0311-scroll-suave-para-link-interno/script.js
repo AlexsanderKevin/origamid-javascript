ec491e5f2f336744ea54c4bd3cf3974e69476343
ChesterKing66 <kevin13348@gmail.com>
2022-01-04 19:31:20 -0300

    0311 scroll suave link interno

---
+++ aulas/modulo-03-DOM-para-iniciantes/0311-scroll-suave-para-link-interno/script.js
@@ -0,0 +1,62 @@
+function initTabNav(){
+
+    const tabMenu = document.querySelectorAll('.js-tabmenu li')
+    const tabContent = document.querySelectorAll('.js-tabcontent section')
+    tabContent[0].classList.add('ativo')
+    
+    if(tabMenu.length && tabContent.length){
+    
+        function activeTab(index) {
+            tabContent.forEach(section => {
+                section.classList.remove('ativo')
+            })
+            tabContent[index].classList.add('ativo')
+        }
+        
+        tabMenu.forEach((itemMenu, index) =>{
+            itemMenu.addEventListener('click', ()=> activeTab(index))
+        })
+    }
+}
+initTabNav()
+
+function initAccordion () {
+
+    const accordionList = document.querySelectorAll('.js-accordion dt')
+    const activeClass = 'ativo'
+
+    if(accordionList.length){
+        accordionList[0].classList.add(activeClass)
+        accordionList[0].nextElementSibling.classList.add(activeClass)
+        
+        function activeAccordion(){
+            this.classList.toggle(activeClass)
+            this.nextElementSibling.classList.toggle(activeClass)
+        }
+        
+        accordionList.forEach(item => item.addEventListener('click', activeAccordion))
+    }
+}
+initAccordion()
+
+function initScrollSuave(){
+
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+    
+    function scrollToSection(event){
+        event.preventDefault()
+    
+        const href = event.currentTarget.getAttribute('href')
+        const section = document.querySelector(href)
+    
+        section.scrollIntoView({ // this puts the target element into user's view
+            behavior: 'smooth', // this sets how the transition will behave
+            block: 'start', // this sets where exactly the view will stop from target component
+        })
+    }
+    
+    linksInternos.forEach(link => 
+        link.addEventListener('click', scrollToSection)
+    )
+}
+initScrollSuave()
