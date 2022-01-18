10c7d75c3f161085ec5486b477de6e9c3f5d8be0
kevin alexsander <kevin13348@gmail.com>
2022-01-18 15:39:48 -0300

    0503 Modal

--- aulas/modulo-05-efeitos-no-DOM/0503-modal/js/script.js
+++ aulas/modulo-05-efeitos-no-DOM/0503-modal/js/script.js
@@ -3,9 +3,11 @@ import initScrollSuave from './modules/scroll-suave.js'
 import initAnimacaoScroll from './modules/scroll-animacao.js'
 import initAccordion from './modules/accordion.js'
 import initTabNav from './modules/tabnav.js'
+import initModal from './modules/modal.js'
 
 // nota: as funções abaixo carregam de forma ASSINCRONA, ou seja, não esperam que a de cima carregue para carregarem, porém os importes serão executados em ordem SINCRONA
 initScrollSuave()
 initAnimacaoScroll()
 initAccordion()
 initTabNav()
+initModal()
