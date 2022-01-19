384708355b4a257059e3411fe96896e741d09475
kevin alexsander <kevin13348@gmail.com>
2022-01-19 13:27:14 -0300

    0505 Bubble Event

---
+++ aulas/modulo-05-efeitos-no-DOM/0505-event-bubble/js/script.js
@@ -0,0 +1,15 @@
+//nota: tudo está sendo indexado neste arquivo
+import initScrollSuave from './modules/scroll-suave.js'
+import initAnimacaoScroll from './modules/scroll-animacao.js'
+import initAccordion from './modules/accordion.js'
+import initTabNav from './modules/tabnav.js'
+import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
+
+// nota: as funções abaixo carregam de forma ASSINCRONA, ou seja, não esperam que a de cima carregue para carregarem, porém os importes serão executados em ordem SINCRONA
+initScrollSuave()
+initAnimacaoScroll()
+initAccordion()
+initTabNav()
+initModal()
+initTooltip()
