a94132ff2c3fb0ca66aa053b642be47cb5d5979f
kevin alexsander <kevin13348@gmail.com>
2022-01-20 16:07:07 -0300

    0507 Outside Click

---
+++ aulas/modulo-05-efeitos-no-DOM/0507-outside-click/js/modules/outsideclick.js
@@ -0,0 +1,23 @@
+export default function outsideClick(element, events, callback){
+    const html = document.documentElement
+    const outside = 'data-outside' // nota: essa propriedade será utilizada para que seja possível saber se o elemento já possui o event listener
+
+    if(!element.hasAttribute(outside)){
+        events.forEach(userEvent => {
+            html.addEventListener(userEvent, handleOutsideClick)
+        })
+        element.setAttribute(outside, '')
+    } // nota: isso verifica se o elemento ja tem o event listener para não adicionar mais de um
+
+    function handleOutsideClick(event){
+        if(!element.contains(event.target)){
+            element.removeAttribute(outside) // nota: esse atributo deve ser removido para que o event listener possa ser adicionado de novo depois
+            events.forEach(userEvent => {
+                html.removeEventListener('click', handleOutsideClick)
+            })
+
+            callback()
+            // nota: isso remove o eventListener ao fechar o menu
+        }
+    }
+}
