a479d811338bba9bbfaf4759514faf243988b88d
kevin alexsander <kevin13348@gmail.com>
2022-01-20 15:13:30 -0300

    0506 Bubble Event

--- aulas/modulo-05-efeitos-no-DOM/0506-event-bubble/slide.js
+++ aulas/modulo-05-efeitos-no-DOM/0506-event-bubble/slide.js
@@ -1,2 +1,17 @@
 // Buble Event
     // Buble: verifica se o elemento parent possui o evento que foi disparado no elemento filho. Caso não possua, segue-se para o próximo
+
+    // Sequencia do Event Buble
+        // Hoisting: 
+            // As variáveis e constantes são alocadas na memória, uma por uma, mas nenhum valor é atribuido. Essa característica chama-se 'Temporary Dead Zone'.
+            // As funções também são alocadas, mas já com todo o seu corpo atribuído.
+
+        // Execução: 
+            // Os métodos passam para a Call Stack
+            // Os métodos são executados e os valores são atribuídos às variáveis
+            // Modifica a Web Api para ligar eventos a elementos, por exemplo
+
+        // Buble: 
+            //verifica se o elemento parent possui o evento que foi disparado no elemento filho. Caso não possua, segue-se para o próximo
+        
+        // Script finalizado.
