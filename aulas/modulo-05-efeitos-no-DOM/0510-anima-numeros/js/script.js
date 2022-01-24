b54633644708ded43658f53c995ee2dcfeb532a1
kevin alexsander <kevin13348@gmail.com>
2022-01-24 11:03:57 -0300

    0510 Anima Números

---
+++ aulas/modulo-05-efeitos-no-DOM/0510-anima-numeros/js/script.js
@@ -0,0 +1,21 @@
+//nota: tudo está sendo indexado neste arquivo
+import initScrollSuave from './modules/scroll-suave.js'
+import initAnimacaoScroll from './modules/scroll-animacao.js'
+import initAccordion from './modules/accordion.js'
+import initTabNav from './modules/tabnav.js'
+import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
+import initDropdownMenu from './modules/dropdown-menu.js'
+import initMenuMobile from './modules/menu-mobile.js'
+import initAnimaNumeros from './modules/anima-numeros.js'
+
+// nota: as funções abaixo carregam de forma ASSINCRONA, ou seja, não esperam que a de cima carregue para carregarem, porém os importes serão executados em ordem SINCRONA
+initScrollSuave()
+initAnimacaoScroll()
+initAccordion()
+initTabNav()
+initModal()
+initTooltip()
+initDropdownMenu()
+initMenuMobile()
+initAnimaNumeros()
