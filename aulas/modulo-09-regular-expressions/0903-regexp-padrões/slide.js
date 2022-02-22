8c1a540925ae452dc19948f6aa41aedfda795515
kevin alexsander <kevin13348@gmail.com>
2022-02-22 14:26:13 -0300

    0903 Regexp Padrões

---
+++ aulas/modulo-09-regular-expressions/0903-regexp-padrões/slide.js
@@ -0,0 +1,55 @@
+const CEP = slide_2 => {
+
+    // CEP
+
+    const regexpCEP = /\d{5}[-\s]?\d{3}/g
+
+    const ceps = [
+        '00000-000',
+        '00000 000',
+        '00000000'
+    ]
+
+    for(const cep of ceps){
+        console.log(cep, cep.match(regexpCEP))
+    }
+}
+
+const CPF = slide_3 => {
+
+    // CPF
+
+    const regexpCPF = /(?:\d{3}[-.]?){3}\d{2}/g
+
+    const cpfs = [
+        '000.000.000-00',
+        '000-000-000-00',
+        '000.000.000.00',
+        '000000000-00',
+        '00000000000'
+    ]
+
+    for(const cpf of cpfs){
+        console.log(cpf, cpf.match(regexpCPF))
+    }
+}
+
+const CNPJ = slide_4 => {
+
+    // CNPJ
+
+    const regexpCNPJ = /\d{2}[-.]?(?:\d{3}[-.]?){2}[-\/]?\d{4}[-.]?\d{2}/g
+
+    const cnpjs = [
+        '00.000.000/0000-00',
+        '00000000000000',
+        '00-000-000-0000-00',
+        '00.000.000./000000',
+        '00.000.000.000000',
+        '00.000.000.0000.00'
+    ]
+
+    for(const cnpj of cnpjs){
+        console.log(cnpj, cnpj.match(regexpCNPJ))
+    }
+}
