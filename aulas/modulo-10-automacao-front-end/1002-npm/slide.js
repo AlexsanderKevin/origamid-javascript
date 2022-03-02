7eb0e4627c4275f4d89ebb498a11fbbbbcf0a056
kevin alexsander <kevin13348@gmail.com>
2022-03-02 10:56:53 -0300

    1002 NPM

---
+++ aulas/modulo-10-automacao-front-end/1002-npm/slide.js
@@ -0,0 +1,54 @@
+const NPM_Node_Package_Manager = slide_2 => {
+
+    // NPM (Node Package Manager)
+
+    // Gerenciador de Pacotes
+        // Feito para Node.js
+
+    // Linha de comando
+        // $ npm install lodash
+
+    // https://www.npmjs.com/
+}
+
+const Instalar_Pacotes = slide_3 => {
+
+    // Instalar Pacotes
+
+    // $ npm install lodash
+        // Instala o pacote lodash no diretório atual da linha de comando
+
+    // $ npm install eslint -g
+        // Instala o eslint globalmente (No mac é necessário o sudo para instalar globalmente).
+
+    // $ npm update lodash
+        // Atualiza o pacote lodash
+
+    // $ npm uninstall lodash
+        // Desinstala o pacote lodash
+}
+const Package_json = slide_4 => {
+
+    // Packaege.json
+
+    // package.json
+        // Arquivo local com as configurações e dependências de pacotes NPM
+
+    // $ npm init 
+        // Inicia uma nova configuração local do npm
+
+    // $ npm install 
+        // Instala todas as dependências listadas no arquivo package.json
+}
+
+const Arquivos_Invisíveis = slide_5 => {
+
+    // Arquivos Invisiveis
+
+    // Windows
+        // Exibir > Itens Ocultos
+
+    // Mac
+        // $ defaults write com.apple.Finder AppleShowAllFiles true
+        // $ killall Finder 
+}
