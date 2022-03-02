d59ce1956a446de220a615e82fc3af13298755f1
kevin alexsander <kevin13348@gmail.com>
2022-03-02 10:29:39 -0300

    1001 Linha de Comando

---
+++ aulas/modulo-10-automacao-front-end/1001-linha-de-comando/slide.js
@@ -0,0 +1,57 @@
+const Linha_de_Comando_CLI = slide_2 => {
+
+    // Linha de Comando (CLI)
+
+    // Interagir com o Computador através de texto
+        // Comando começando com $(Unix) ou > (Win)
+
+    // UNIX (Mac e Linux) vs Windows
+        // Bash e Zsh (Unix) | CMD e PowerShell (Windows)
+
+    // https://ss64.com
+}
+
+const Movimentar_Bash = slide_3 => {
+
+    // Movimentar (Bash)
+
+    // $ cd sites
+        // Move para a pasta sites
+
+    // $ cd ..
+        // Move para a pasta anterior 
+
+    // $ cd ~
+        // Move para o diretório principal do sistema / home
+}
+
+const Listar_e_Criar_Bash = slide_4 => {
+
+    // Listar e Criar (Bash)
+
+    // $ ls -all
+        // Lista de diretórios, arquivos invisíveis e detalhes
+
+    // $ mkdir <novo-repositorio>
+        // Cria o diretorio com o nome escrito
+
+    // $ touch index.html
+        // Cria o arquivo index.html 
+}
+
+const Remover_Bash = slide_5 => {
+
+    // Remover (Bash)
+
+    // $ rm index.html
+        // Remove o arquivo index.html
+
+    // $ rm -r site
+        // Remove o diretório "site" e todos os arquivos dentro dele
+
+    // seta para cima / baixo
+        // Acessa comandos anteriores
+
+    // tab
+        // Auto-completa o comando
+}
