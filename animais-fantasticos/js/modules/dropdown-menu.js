b97780e2aa789fec357afd1a5659144b6edeecb8
Kevin Alexsander De Jesus1 <kjesus1@Lenovo.com>
2022-06-27 15:48:27 -0300

    aula-1112 Refatorar Dropdown Menu END

--- animais-fantasticos/js/modules/dropdown-menu.js
+++ animais-fantasticos/js/modules/dropdown-menu.js
@@ -1,23 +1,40 @@
 import outsideClick from './outsideclick.js';
 
-export default function initDropdownMenu() {
-    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+export default class DropdownMenu {
+    constructor(dropdown, events) {
+        this.dropdownMenus = document.querySelectorAll(dropdown);
+        this.activeClass = 'active';
+        // define 'touchstart' e 'click' como argumento padrão para 'events' caso o usuário não defina
+        if (events === undefined) this.events = ['touchstart', 'click'];
+        else this.events = events;
 
-    function handleClick(event) {
+        this.activeMenu = this.activeMenu.bind(this);
+    }
+
+    // Ativa o dropdown menu e adiciona a função que observa o clique fora dele
+    activeMenu(event) {
         event.preventDefault();
-        this.classList.add('active');
+        const target = event.currentTarget;
 
-        outsideClick(this, ['touchstart', 'click'], () => {
-            this.classList.remove('active');
+        target.classList.add(this.activeClass);
+
+        outsideClick(target, this.events, () => {
+            this.classList.remove(this.activeClass);
         });
     }
 
-    dropdownMenus.forEach((menu) => {
-        ['touchstart', 'click'].forEach((userEvent) => {
-            menu.addEventListener(userEvent, handleClick);
+    addMenuEvent(){
+        this.dropdownMenus.forEach((menu) => {
+            this.events.forEach((userEvent) => {
+                menu.addEventListener(userEvent, this.activeMenu);
+            });
+            // nota: para atribuir uma mesma função a varios eventos é possível atribuir tais eventos a um vetor e, para cada evento listado, aplicar o método.
         });
-        // nota: para atribuir uma mesma função a varios eventos é possível
-        // atribuir tais eventos a um vetor e, para cada evento listado,
-        // aplicar o método.
-    });
+    }
+
+    init(){
+        this.addMenuEvent();
+
+        return this;
+    }
 }
