cb4b3a3ade9a302efa20be75ee64970164c5d310
ChesterKing66 <kevin13348@gmail.com>
2022-06-28 21:12:37 -0300

    aula-1114 Refatorar Funcionamento END

--- animais-fantasticos/js/modules/funcionamento.js
+++ animais-fantasticos/js/modules/funcionamento.js
@@ -1,20 +1,41 @@
-export default function initFuncionamento() {
-    const funcionamento = document.querySelector('[data-semana]');
-    const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
-    // nota: passar o construtor 'Number' para o método .map() converte os
-    // dados da string alvo para number.
-    const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+export default class Funcionamento {
+    constructor(semana) {
+        this.funcionamento = document.querySelector(semana);
+        this.activeClass = 'aberto'
+    }
 
-    const dataAgora = new Date();
-    const diaAgora = dataAgora.getDay();
-    const horarioAgora = dataAgora.getHours();
+    dadosFuncionamento(){
+        this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number);
+        // nota: passar o construtor 'Number' para o método .map() converte os
+        // dados da string alvo para number.
+        this.horarioSemana = this.funcionamento.dataset.horario.split(',').map(Number);
+    }
 
-    const semanaAberto = diasSemana.includes(diaAgora);
-    const horarioAberto = horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
+    dadosAgora(){
+        this.dataAgora = new Date();
+        this.diaAgora = this.dataAgora.getDay();
+        this.horarioAgora = this.dataAgora.getUTCHours() - 3;
+    }
 
-    if (semanaAberto && horarioAberto) {
-        funcionamento.classList.add('aberto');
-    } else {
-        funcionamento.classList.remove('aberto');
+    estaAberto(){
+        const semanaAberto = this.diasSemana.includes(this.diaAgora);
+        const horarioAberto = this.horarioAgora >= this.horarioSemana[0] && this.horarioAgora < this.horarioSemana[1];
+
+        return semanaAberto && horarioAberto;
+    }
+
+    abrir(){
+        if (this.estaAberto()) {
+            this.funcionamento.classList.add(this.activeClass);
+        } else {
+            this.funcionamento.classList.remove(this.activeClass);
+        }
+    }
+
+    init(){
+        this.dadosFuncionamento();
+        this.dadosAgora();
+        this.abrir();
+        return this;
     }
 }
